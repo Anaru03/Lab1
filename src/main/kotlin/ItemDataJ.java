056f9542cf3c18d4b3728fae68ae8f0a2932ac02
Anaru03 <dele22428@uvg.edu.gt>
2023-07-15 20:55:42 -0600

    Java finalizado

--- src/main/kotlin/ItemDataJ.java
+++ src/main/kotlin/ItemDataJ.java
@@ -28,5 +28,26 @@ public class ItemDataJ {
     }
 
 
+    public String getInfo() {
+        if (originalValue instanceof String) {
+            return "L" + ((String) originalValue).length();
+        } else if (originalValue instanceof Integer) {
+            int value = (Integer) originalValue;
+            if (value % 10 == 0) {
+                return "M10";
+            } else if (value % 5 == 0) {
+                return "M5";
+            } else if (value % 2 == 0) {
+                return "M2";
+            } else {
+                return null;
+            }
+        } else if (originalValue instanceof Boolean) {
+            boolean value = (Boolean) originalValue;
+            return value ? "Verdadero" : "Falso";
+        } else {
+            return null;
+        }
+    }
 
 }
